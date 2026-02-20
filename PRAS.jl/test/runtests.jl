a8c3e7fea6d0b348910af932f21446feeb725255
Abdelrahman Ayad <abdelrahman.ayad@gmail.com>
2026-02-20 01:05:10 -0700

    add ShortfallResult tests

--- PRAS.jl/test/runtests.jl
+++ PRAS.jl/test/runtests.jl
@@ -10,10 +10,19 @@ using Test
     lole = LOLE(sf)
     neue = NEUE(sf)
 
+    alpha = 0.95
+    cvar = CVAR(sf, alpha)
+    ncvar = NCVAR(sf, cvar)
+
     @test val(eue) isa Float64
     @test stderror(eue) isa Float64
     @test val(neue) isa Float64
     @test stderror(neue) isa Float64
+    @test val(cvar) isa Float64
+    @test stderror(cvar) isa Float64
+    @test val(ncvar) isa Float64
+    @test stderror(ncvar) isa Float64
+
 end
 
 @testset "ShortfallSamplesResult" begin
